e2da7b4153846c24699ff1570302136af151e168
Josh Newsom <newsommeister@gmail.com>
2016-04-10 18:24:05 -0600

    Initial commit - basic slider functionality

---
+++ app.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+    // TODO: Randomize starting grid
+    var grid = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+    $('div.cell').on('click', function() {
+        var $row = $(this).attr('class').match(/row\d/)[0][3];
+        var $col = $(this).attr('class').match(/col\d/)[0][3];
+        var $emptyRow = $('div.empty').attr('class').match(/row\d/)[0][3];
+        var $emptyCol = $('div.empty').attr('class').match(/col\d/)[0][3];
+
+        if ($row !== $emptyRow && $col !== $emptyCol) return;
+
+        switch ($row - $emptyRow) {
+            case -1:
+                $(this).animate({
+                    'top': '+=33.333%'
+                }, 'fast');
+                $(this).removeClass('row' + $row);
+                $(this).addClass('row' + (Number($row) + 1));
+                $('div.empty').removeClass('row' + $emptyRow);
+                $('div.empty').addClass('row' + (Number($emptyRow) - 1));
+                break;
+            case 1:
+                $(this).animate({
+                    'top': '-=33.333%'
+                }, 'fast');
+                $(this).removeClass('row' + $row);
+                $(this).addClass('row' + (Number($row) - 1));
+                $('div.empty').removeClass('row' + $emptyRow);
+                $('div.empty').addClass('row' + (Number($emptyRow) + 1));
+                break;
+        }
+
+        switch ($col - $emptyCol) {
+            case -1:
+                $(this).animate({
+                    'left': '+=33.333%'
+                }, 'fast');
+                $(this).removeClass('col' + $col);
+                $(this).addClass('col' + (Number($col) + 1));
+                $('div.empty').removeClass('col' + $emptyCol);
+                $('div.empty').addClass('col' + (Number($emptyCol) - 1));
+                break;
+            case 1:
+                $(this).animate({
+                    'left': '-=33.333%'
+                }, 'fast');
+                $(this).removeClass('col' + $col);
+                $(this).addClass('col' + (Number($col) - 1));
+                $('div.empty').removeClass('col' + $emptyCol);
+                $('div.empty').addClass('col' + (Number($emptyCol) + 1));
+                break;
+        }
+    });
+});
