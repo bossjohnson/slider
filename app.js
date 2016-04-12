8bf70d5afc215e0958f5cdd1a027aeb298e671cc
Josh Newsom <newsommeister@gmail.com>
2016-04-11 22:27:25 -0600

    Added Grumpy Cat image and setUpImage function

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
+var setUpImage = function() {
+    var $conH = $('#container').height();
+    var $conW = $('#container').width();
+    $('#d2').css('background-position', $conW * -0.33333 + 'px 0');
+    $('#d3').css('background-position', $conW * -0.66666 + 'px 0');
+    $('#d4').css('background-position', '0 ' + $conH * -0.33333 + 'px');
+    $('#d5').css('background-position', $conW * -0.33333 + 'px ' + $conH * -0.33333 + 'px');
+    $('#d6').css('background-position', $conW * -0.66666 + 'px ' + $conH * -0.33333 + 'px');
+    $('#d7').css('background-position', '0 ' + $conH * -0.66666 + 'px');
+    $('#d8').css('background-position', $conW * -0.33333 + 'px ' + $conH * -0.66666 + 'px');
+};
+
 $(document).ready(function() {
+  setUpImage();
     // TODO: Add image to cells
     // TODO: Randomize starting positions
     // TODO: Add functionality to check puzzle completion
-    var grid = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
-    ];
+
+
+    $(window).on('resize', setUpImage);
+
     $('div.cell').on('click', function() {
         var $row = $(this).attr('class').match(/row\d/)[0][3];
         var $col = $(this).attr('class').match(/col\d/)[0][3];
